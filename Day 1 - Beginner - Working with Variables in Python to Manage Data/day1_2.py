a48b9237033818ba3f27d70b330cfb60777823a9
Grigorii <hopelynxstudio@gmail.com>
2024-09-23 16:50:52 +0800

    Day1 compete, skipped some tasks

---
+++ Day 1 - Beginner - Working with Variables in Python to Manage Data/day1_2.py
@@ -0,0 +1,5 @@
+print("Welcome to bands name generator! You could try to generate possible name for your band by answering couple simple questions!")
+city_name = input("What is the name of the city you grew up in?\n")
+pet_name = input("What was your favorite pets name?\n")
+
+print("Your band's name could have been: " + '"' + city_name + " " + pet_name + '"')
---
+++ README.md
@@ -0,0 +1,3 @@
+***It might become a Journey***
+
+My wish is to try to handle this challenge and comlete it in 100 days or less
