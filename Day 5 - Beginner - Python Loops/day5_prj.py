5e0eff13a91a3094368839e8ee8cd6877545fae5
Grigorii <hopelynxstudio@gmail.com>
2024-09-27 00:12:00 +0800

    added functionality for the password generator

--- Day 5 - Beginner - Python Loops/day5_prj.py
+++ Day 5 - Beginner - Python Loops/day5_prj.py
@@ -1,6 +1,6 @@
 #Password Generator Project
 
-
+import csv
 import random
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -18,14 +18,23 @@ def generate_password(nr_letters, nr_symbols, nr_numbers):
         password.insert(random.randint(0,len(password)),random.choice(numbers))
     return ''.join(password)
 
+def save_to_file(password,website_name):
+    with open('passwords.csv', 'a') as myfile:
+        # writer = csv.writer(myfile, delimiter =',')
+        myfile.write(",".join([password,website_name]))
+        myfile.write("\n")
+    print("saved!")
+
 
 def main():
     print("Welcome to the PyPassword Generator!")
     nr_letters= int(input("How many letters would you like in your password?\n")) 
     nr_symbols = int(input(f"How many symbols would you like?\n"))
     nr_numbers = int(input(f"How many numbers would you like?\n"))
-    print(generate_password(nr_letters, nr_symbols, nr_numbers))
-
+    generated = generate_password(nr_letters, nr_symbols, nr_numbers)
+    website_name = input(f"What is website name?\n")
+    save_to_file(generated, website_name)
+    print(generated)
 
 if __name__ == "__main__":
     main()
